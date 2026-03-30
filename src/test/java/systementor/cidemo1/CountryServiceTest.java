5ec3ec9d1fb0e7be99b8d2c5453ace3d44139192
KZTP47 <karokh550@hotmail.com>
2026-03-30 15:57:11 +0200

    Set up Mockito for CountryServiceTest
    Added Mockito dependencies and test annotations for mocking.

--- src/test/java/systementor/cidemo1/CountryServiceTest.java
+++ src/test/java/systementor/cidemo1/CountryServiceTest.java
@@ -1,5 +1,22 @@
 package systementor.cidemo1;
 
+//la till detta för att kunna göra mockning 
+//package systementor.cidemo1; DENNA FANNS JU REDAN
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
+//
+
 
 
 public class CountryServiceTest {
